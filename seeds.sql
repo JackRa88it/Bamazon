3bad65628a87e131b910b7f4f3f061e55ae28250
Jack <jhkent88@gmail.com>
2018-09-06 14:43:06 -0700

    Fix schema and seeds.sql, need add product

--- seeds.sql
+++ seeds.sql
@@ -1,5 +1,5 @@
 USE bamazon;
 
--- INSERT INTO products (product_name, dept_name, price, stock_qty) VALUES ('rubber chicken', 'toys', 5, 100), ('plastic trumpet', 'music', 200, 100);
+INSERT INTO products (name, id_departments, price, stock_qty) VALUES ('rubber chicken', 2, 5, 4), ('plastic trumpet', 1, 200, 100), ('nintendo switch', 2, 200, 100), ('manuscript paper', 1, 10, 3), ('slow cooker', 3, 50, 100), ('8in chef knife', 3, 30, 100), ('bread knife', 3, 20, 100), ('pro controller', 2, 80, 100), ('4 person tent', 4, 200, 100), ('camping stove', 4, 50, 100);
 
-INSERT INTO departments (dept_name, overhead_costs) VALUES ('music', 1000), ('toys', 1000), ('kitchen', 1000), ('outdoors', 1000);
+INSERT INTO departments (name, overhead) VALUES ('music', 1000), ('toys', 1000), ('kitchen', 1000), ('outdoors', 1000);
